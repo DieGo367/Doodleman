be9b902f9fbc04ba38e1f51a4ec05b7c4591810b
DieGo <diegog367@gmail.com>
2018-09-15 18:33:33 -0700

    Fix bug with wave spawning

--- scripts/gamemodes/survival.js
+++ scripts/gamemodes/survival.js
@@ -1,7 +1,9 @@
 const SurvivalMode = {
+  ready: false,
   wave: 0,
   score: 0,
   start: function() {
+    this.ready = false;
     ResourceManager.request("levels/Dungeon-0.json",function(data) {
       Level.load(data);
     });
@@ -9,8 +11,11 @@ const SurvivalMode = {
   onLevelLoad: function() {
     addPlayer(0);
     if (multiplayer) addPlayer(1);
+    this.ready = true;
   },
   tick: function() {
+    if (!this.ready) return;
+    
     if (PaintMinion.getAll().length==0) {
       this.wave++;
       this.spawnWave(this.wave);
