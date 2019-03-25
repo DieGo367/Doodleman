15ed86087f44828fa8fc5454d7ee12d9a4ef3520
DieGo <diegog367@gmail.com>
2019-03-25 15:23:20 -0700

    Prevent file caching for faster updates

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from flask import render_template
 from flask import send_file
 from flask import send_from_directory
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 scripts = [
   "peer.min.js",
