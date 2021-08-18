95a584042d6743940c32270986584a61c32840c9
DieGo <diegog367@gmail.com>
2021-08-17 18:57:33 -0700

    load level from url param

--- src/doodleman/Doodleman.js
+++ src/doodleman/Doodleman.js
@@ -11,7 +11,10 @@ class DoodlemanGame extends Scribble.Game {
 		this.engine.setAirResistance(0.92);
 		this.engine.loadActorData("data/actors.json").then(() => {
 			this.engine.registerClasses(DMOs);
-			this.engine.level.set("levels/1st Platformy Level.json");
+			let url = new URL(window.location.href);
+			let level = url.searchParams.get("level");
+			if (level) this.engine.level.set("levels/"+level+".json");
+			else this.engine.level.set("levels/1st Platformy Level.json");
 			// this.tempTestBlocks();
 		});
 		document.addEventListener("mousemove", e => this.onmousemove(e));
