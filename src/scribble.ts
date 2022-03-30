48ec1cfcf5ca9b29cfa08c1da8810451669c7126
DieGo <diegog367@gmail.com>
2022-03-30 15:52:13 -0700

    Simplify Scribble exports definition

--- src/scribble.ts
+++ src/scribble.ts
@@ -1,37 +1,15 @@
-import {
-	RIGHT, LEFT, CENTER, EDGE, TERRAIN, COLOR, SHAPE,
-	Pt, Circle, Box, Line, Polygon, fillShape, HiddenCanvas
-} from "./scribble/util.js";
-import { Engine } from "./scribble/engine.js";
-import { ResourceManager } from "./scribble/resource.js";
-import * as Collision from "./scribble/collision.js";
-import { ObjectManager, GameObject, Objects } from "./scribble/object.js";
-import { Game } from "./scribble/game.js";
-import { ImageManager } from "./scribble/images.js";
-import { SoundManager } from "./scribble/sounds.js";
-import { AnimationManager, AnimationSheet, AnimationComponent } from "./scribble/animations.js";
-import { LevelManager, BlankLevel } from "./scribble/level.js";
-import { Camera } from "./scribble/camera.js";
-import { InputManager } from "./scribble/input.js";
-import { Backgrounds } from "./scribble/backgrounds.js";
-import { FileLoader } from "./scribble/file.js";
-import { Debug } from "./scribble/debug.js";
-
-export {
-	RIGHT, LEFT, CENTER, EDGE, TERRAIN, COLOR, SHAPE,
-	Pt, Circle, Box, Line, Polygon, fillShape, HiddenCanvas,
-	Engine,
-	ResourceManager,
-	Collision,
-	ObjectManager, GameObject, Objects,
-	Game,
-	ImageManager,
-	SoundManager,
-	AnimationManager, AnimationSheet, AnimationComponent,
-	LevelManager, BlankLevel,
-	Camera,
-	InputManager,
-	Backgrounds,
-	FileLoader,
-	Debug
-};
+export * from "./scribble/util.js";
+export * from "./scribble/engine.js";
+export * from "./scribble/resource.js";
+export * as Collision from "./scribble/collision.js";
+export * from "./scribble/object.js";
+export * from "./scribble/game.js";
+export * from "./scribble/images.js";
+export * from "./scribble/sounds.js";
+export * from "./scribble/animations.js";
+export * from "./scribble/level.js";
+export * from "./scribble/camera.js";
+export * from "./scribble/input.js";
+export * from "./scribble/backgrounds.js";
+export * from "./scribble/file.js";
+export * from "./scribble/debug.js";
