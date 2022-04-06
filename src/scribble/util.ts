a3a44f09db1753ab39404f4b8bb5c8af54634a53
DieGo <diegog367@gmail.com>
2022-04-06 13:43:32 -0700

    Utility validate function for type guards

--- src/scribble/util.ts
+++ src/scribble/util.ts
@@ -70,3 +70,164 @@ export const Angle = {
 			return angle <= end || start <= angle;
 	}
 }
+
+type primitiveName = "undefined" | "boolean" | "number" | "bigint" | "string" | "symbol" | "function" | "object";
+export interface ValidationRule {
+	test: (string | number)[] | "*";
+	optional?: boolean;
+	then?: ValidationRule[];
+	is?: primitiveName | ValidationRule[] | {new(...args: any[]): any};
+	equals?: any;
+	in?: any[];
+	keyOf?: any;
+	arrayOf?: primitiveName | ValidationRule[] | {new(...args: any[]): any};
+	mapOf?: primitiveName | ValidationRule[] | {new(...args: any[]): any};
+}
+export function validate(data: unknown, format: ValidationRule[], warn = true, prefix = "{}"): boolean {
+	let fail = (msg: string) => {
+		if (warn) console.warn(`${prefix} > ${msg}`);
+	}
+	if (typeof data !== "object") {
+		fail(`Root value "${data}" was not an object.`);
+		return false;
+	}
+	for (let rule of format) {
+		if (rule.test === "*") rule.test = Object.keys(data);
+		for (let prop of rule.test) {
+			if (!(prop in data) && !rule.optional) {
+				fail(`Missing property "${prop}".`);
+				return false;
+			}
+		}
+		if ("is" in rule) {
+			if (typeof rule.is === "string") {
+				for (let prop of rule.test) {
+					if (!(rule.optional && typeof data[prop] === undefined)) {
+						if (typeof data[prop] !== rule.is) {
+							fail(`Expected property "${prop}" to have type "${rule.is}", but got "${typeof data[prop]}".`);
+							return false;
+						}
+					}
+				}
+			}
+			else if (rule.is instanceof Array) {
+				for (let prop of rule.test) {
+					if (!(rule.optional && typeof data[prop] === undefined)) {
+						if (!validate(data[prop], rule.is, warn, `${prefix}.${prop}`))
+							return false;
+					}
+				}
+			}
+			else {
+				for (let prop of rule.test) {
+					if (!(rule.optional && typeof data[prop] === undefined)) {
+						if (!(data[prop] instanceof rule.is)) {
+							fail(`Expected property "${prop}" to be an instance of "${rule.is.name}".`);
+							return false;
+						}
+					}
+				}
+			}
+		}
+		if ("equals" in rule) for (let prop of rule.test) {
+			if (!(rule.optional && typeof data[prop] === undefined)) {
+				if (data[prop] !== rule.equals) {
+					fail(`Expected property "${prop}" to be (${JSON.stringify(rule.equals)}), but got (${JSON.stringify(data[prop])}).`);
+					return false;
+				}
+			}
+		}
+		if ("in" in rule) for (let prop of rule.test) {
+			if (!(rule.optional && typeof data[prop] === undefined)) {
+				if (!rule.in.includes(data[prop])) {
+					fail(`Expected property "${prop}" to be one of ${JSON.stringify(rule.in)}, but got (${JSON.stringify(data[prop])}).`);
+					return false;
+				}
+			}
+		}
+		if ("keyOf" in rule) for (let prop of rule.test) {
+			if (!(rule.optional && typeof data[prop] === undefined)) {
+				let keys = Object.keys(rule.keyOf);
+				if (!keys.includes(String(data[prop]))) {
+					fail(`Expected property "${prop}" to be one of ${JSON.stringify(keys)}, but got (${JSON.stringify(data[prop])}).`);
+					return false;
+				}
+			}
+		}
+		if ("arrayOf" in rule) for (let prop of rule.test) {
+			if (!(rule.optional && typeof data[prop] === undefined)) {
+				let target: unknown = data[prop];
+				if (target instanceof Array) {
+					if (typeof rule.arrayOf === "string") {
+						for (let item of target as unknown[]) {
+							if (typeof item !== rule.arrayOf) {
+								fail(`Not all items of array "${prop}" were of type "${rule.arrayOf}". (Found "${typeof item}")`);
+								return false;
+							}
+						}
+					}
+					else if (rule.arrayOf instanceof Array){
+						let rules = rule.arrayOf;
+						if (!target.every((item: unknown, i) => validate(item, rules, warn, `${prefix}.${prop}.${i}`)))
+							return false;
+					}
+					else {
+						for (let item of target as unknown[]) {
+							if (!(item instanceof rule.arrayOf)) {
+								fail(`Not all items of array "${prop}" were instances of ${rule.arrayOf.name}.`);
+								return false;
+							}
+						}
+					}
+				}
+				else {
+					fail(`Property ${prop} was not an array.`);
+					return false;
+				}
+			}
+		}
+		if ("mapOf" in rule) for (let prop of rule.test) {
+			if (!(rule.optional && typeof data[prop] === undefined)) {
+				let target: unknown = data[prop];
+				if (typeof target === "object") {
+					if (typeof rule.mapOf === "string") {
+						for (let key in target) {
+							let item: unknown = target[key];
+							if (typeof item !== rule.mapOf) {
+								fail(`Not all items of object "${prop}" were of type "${rule.mapOf}". (Found "${typeof item}")`);
+								return false;
+							}
+						}
+					}
+					else if (rule.mapOf instanceof Array) {
+						for (let key in target) {
+							let item: unknown = target[key];
+							if (!validate(item, rule.mapOf, warn, `${prefix}.${prop}`))
+								return false;
+						}
+					}
+					else {
+						for (let key in target) {
+							let item: unknown = target[key];
+							if (!(item instanceof rule.mapOf)) {
+								fail(`Not all items of object "${prop}" were instances of ${rule.mapOf.name}.`);
+								return false;
+							}
+						}
+					}
+				}
+				else {
+					fail(`Property "${prop}" was not an object.`);
+					return false;
+				}
+			}
+		}
+		if ("then" in rule) for (let prop of rule.test) {
+			if (typeof data[prop] !== undefined) {
+				if (!validate(data, rule.then, warn, `${prefix}#`))
+					return false;
+			}
+		}
+	}
+	return true;
+}
